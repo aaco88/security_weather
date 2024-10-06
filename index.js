b0cf29d95845181df221b488245332dffdb4976c
John Connor <jconnor@mail.com>
2024-10-06 20:58:33 -0300

    Modificado

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // const axios = require('axios');
-import axios from 'axios';
+/* import axios from 'axios';
 
 const getWeather = async (city) => {
    const apiKey = process.env.WEATHER_API_KEY; // La clave API se manejará como un Secret en GitHub
@@ -10,4 +10,22 @@ const getWeather = async (city) => {
 
 getWeather('Buenos Aires')
    .then(data => console.log(data))
-   .catch(err => console.error(err));
+   .catch(err => console.error(err)); */
+
+   
+
+// const axios = require('axios');
+import axios from 'axios';
+
+const getWeather = async (city) => {
+   try {
+         const apiKey = process.env.WEATHER_API_KEY;  // La clave de la API debe estar en los Secrets o en las variables de entorno
+         const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+         const response = await axios.get(url);
+         console.log(response.data);  // Aquí muestra los datos del clima
+   } catch (error) {
+         console.error('Error fetching weather data:', error);
+   }
+};
+
+getWeather('Buenos Aires');
